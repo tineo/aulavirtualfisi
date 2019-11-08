68fc333667351f166b0027054abfc705a96c692e
Cesar Gutierrez <cesar@tineo.mobi>
2019-11-08 10:14:04 -0500

    login validate

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,21 +1,29 @@
 $(function () {
+    if(localStorage.getItem('sessionid'    )  != undefined || localStorage.getItem('sessionid'    ) != null  ){
+        window.location.replace('page-profile-view.html');
+    }
+
     $("#login").click(function () {
 
         let username = $("input[name=username]").val();
         let password = $("input[name=password]").val();
         //alert("login:"+ username + ":"+ password);
-
-
+        //let username =  "ihc";
+        //let password = "1Hc-1Hc1-1";
         const options = {
             method : 'GET',
             url: 'http://35.226.238.158/moodle/login/token.php?username='+username+'&password='+password+'&service=moodle_mobile_app',
 
         }
         axios(options).then(response => {
-            if(response.data.token != undefined || response.data.token != null || response.data.token != ''  ){
+            console.log(response);
+            if(response.data.token != undefined ){
                 alert('Token: '+response.data.token);
                 localStorage.setItem('sessionid',response.data.token);
                 window.location.replace('page-profile-view.html');
+
+            }else{
+                $('#modal6').modal('show');
             }
         }).catch(error =>{
             console.log(error);
