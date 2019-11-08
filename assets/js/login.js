8aa61af1a0c31e8b23dde387d67ea264192fd435
Cesar Gutierrez <cesar@tineo.mobi>
2019-11-08 03:01:04 -0500

    added axios

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,5 +1,18 @@
 $(function () {
     $("#login").click(function () {
         alert("login");
+
+
+        var resultElement = document.getElementById('login-id');
+        resultElement.innerHTML = '';
+
+        axios.get('http://jsonplaceholder.typicode.com/todos')
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+
+            });
     });
 })
