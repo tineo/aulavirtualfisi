c5d03177d08299d46fd3180a397b80465e0cb5e1
Walter Renzo Muñoz Arellano <wmunozarellano@gmail.com>
2019-11-08 03:27:43 -0800

    Token validation

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,17 +3,20 @@ $(function () {
 
         let username = $("input[name=username]").val();
         let password = $("input[name=password]").val();
-        alert("login:"+ username + ":"+ password);
+        //alert("login:"+ username + ":"+ password);
 
 
+        const options = {
+            method : 'GET',
+            url: 'http://35.226.238.158/moodle/login/token.php?username='+username+'&password='+password+'&service=moodle_mobile_app',
 
-        axios.get('http://35.226.238.158/moodle/login/token.php?username='+username+'&password='+password+'&service=moodle_mobile_app')
-            .then(function (response) {
-                console.log(response);
-            })
-            .catch(function (error) {
-                console.log(error);
-
-            });
+        }
+        axios(options).then(response => {
+            alert('Token:'+response.data.token);
+            localStorage.setItem('sessionid',response.data.token);
+            window.location.replace('page-profile-view.html');
+        }).catch(error =>{
+            console.log(error);
+        });
     });
 })
